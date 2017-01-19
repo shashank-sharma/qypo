8aa3355d3891a4e2dfc7b90fa108a53e6d9c0890
shashank <shashank.sharma98@gmail.com>
2017-01-19 20:58:14 +0530

    Added colors

--- learntype.py
+++ learntype.py
@@ -3,6 +3,36 @@ import time
 import requests
 from random import randint
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+def offlineMode():
+	try:
+		file = open("quotes.txt","r")
+	except:
+		print 'Nothing found in offline data'
+
+def compareResult():
+	file = open("result.txt","r")
+	data = file.read().splitlines()
+	file.close()
+	if data[-1] == max(data):
+		print 'It is your highest score'
+	else:
+		print bcolors.BOLD+bcolors.OKGREEN+'Your highest score is: '+str(max(data))+bcolors.ENDC
+
+def addResult(result):
+	file = open("result.txt","a+")
+	file.write(result+'\n')
+	file.close()
+
 def testingArea(real,typed):
     wordCount = 0
     wrongCount = 0
@@ -37,13 +67,16 @@ def getQuotes():
             else:
                 break
         check.append(str(h[y-1].text))
+        file = open("quotes.txt","a+")
+        file.write(str(h[y-1].text)+'\n')
+        file.close()
         que.append(y)
     return check
 
-print 'LearnType - Make typing interesting'
-print '"Increase your typing speed by reading quotes"'
-raw_input('Press Enter to continue...')
-print '\ntip: Make sure everything is exactly typed same'
+print bcolors.OKBLUE+'\n\n\nQypo - Make typing interesting'+bcolors.ENDC
+print bcolors.OKBLUE+'"Increase your typing speed by reading quotes"'+bcolors.ENDC
+raw_input(bcolors.WARNING+'Press Enter to continue...'+bcolors.ENDC)
+print bcolors.OKBLUE+'\nTip: Make sure everything is exactly typed same.'+bcolors.ENDC
 try:
     text = getQuotes()
 except:
@@ -54,19 +87,24 @@ except:
             text = getQuotes()
             break
         except:
-            x+=1
-            if x == 5:
-                print 'Check your internet connetion'
-                end()
+            xx+=1
+            if xx == 5:
+                print 'Internet is dead I guess'
+                print 'Want to try offline ? [Y]es / [N]o'
+                en = raw_input()
+                if en == 'Y':
+                	offlineMode()
+                elif en == 'N':
+                	print 'Ok Bye'
                 break
-print '\n\nAll ready'
-print 'Starting in '
+print bcolors.BOLD+bcolors.FAIL+'\n\nAll ready'+bcolors.ENDC
+print bcolors.FAIL+'Starting in '+bcolors.ENDC
 time.sleep(1)
-print '3'
+print bcolors.FAIL+'3'+bcolors.ENDC
 time.sleep(1)
-print '2'
+print bcolors.FAIL+'2'+bcolors.ENDC
 time.sleep(1)
-print '1'
+print bcolors.FAIL+'1'+bcolors.ENDC
 time.sleep(1)
 print '\n'
 totalTime = 0
@@ -74,7 +112,9 @@ totalWords = 0
 testingArea = []
 for i in text:
     t = time.time()
-    print i
+    print '--------------------------\n'
+    print bcolors.BOLD+i+bcolors.ENDC
+    print '\n--------------------------'
     test = raw_input()
     print time.time() -t
     totalTime += time.time() -t
@@ -93,15 +133,18 @@ for i in xrange(len(text)):
                 else:
                     wordc += 1
             else:
-                print text[i][j],'--------',testingArea[i][j]
-                print text[i],'--------',testingArea[i]
                 wrongc += 1
         except:
             wrongc += 1
-grosswpm = ((wordc+wrongc)/5)/(totalTime/60)
-print '\n\n\nTotal time: ', totalTime
-print 'Gross WPM:',grosswpm
-print 'Total Errors:',wrongc
-print 'Total words:',wordc
-netwpm = grosswpm - (wrongc/(totalTime/60))
-print '\nOverall your Typing Speed is:',netwpm
+if wrongc >= 1:
+	print bcolors.BOLD+bcolors.FAIL+'\n\nYou failed. Better luck next time'+bcolors.ENDC
+else:
+	grosswpm = ((wordc+wrongc)/5)/(totalTime/60)
+	print bcolors.OKGREEN+'\n\n\nTotal time: ' + str(totalTime)+bcolors.ENDC
+	print bcolors.OKGREEN+'Gross WPM: '+ str(grosswpm)+bcolors.ENDC
+	print bcolors.FAIL+'Total Errors: '+str(wrongc)+bcolors.ENDC
+	print bcolors.OKGREEN+'Total words:' +str(wordc)+bcolors.ENDC
+	netwpm = grosswpm - (wrongc/(totalTime/60))
+	print bcolors.BOLD+bcolors.OKGREEN+'\nOverall your Typing Speed is: '+str(netwpm)+'\n\n'+bcolors.ENDC
+        addResult(str(netwpm))
+        compareResult()
